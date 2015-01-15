ecf465a043deaaadd01d82e43dfaed132df9bd22
Barney Fox <barney@lostmy.name>
2015-01-15 18:12:36 +0000

    added collapsible docs

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,7 @@ require('codemirror/mode/htmlmixed/htmlmixed');
 require('codemirror/mode/javascript/javascript');
 require('../../node_modules/lmn.jester.component.swiper/src/js/jester.component.swiper');
 var Video = require('../../node_modules/lmn.jester.component.video/src/js/jester.component.video');
+var Collapsible = require('../../node_modules/lmn.jester.component.collapsible/src/js/jester.component.collapsible');
 
 $(document).ready(function () {
 
@@ -31,5 +32,10 @@ $(document).ready(function () {
   if ($video.length > 0) {
     this.video = new Video('video');
   }
+
+  $.each($('.collapsible'), function (i, el) {
+    var $el = $(el);
+    this.collapsible = new Collapsible(el);
+  });
 
 });
